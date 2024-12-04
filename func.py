1c2e57faddb8696e1dfe09b6c5f5fabb15065e11
apsla <aps.laksha@gmail.com>
2024-12-04 18:17:52 +0100

    Create 1 files: func.ry

---
+++ func.py
@@ -0,0 +1,4 @@
+def input(txt):
+    return txt
+
+# print(input(txt=))
--- math.py
+++ math.py
@@ -1,1 +1,11 @@
 import math
+
+def add(a, b):
+    return a + b
+
+print(add(4, 5))
+
+def minus(a, b):
+    return a - b
+
+print(minus(10, 5))
